180ca8d9df9324f3a4b4fdb3c39bc11ec851aa08
user.email <neo@neoprotocol.space>
2026-02-25 15:49:17 -0300

    fix: remove exposed Infura API key

--- public/assets/neo.config.js
+++ public/assets/neo.config.js
@@ -7,6 +7,6 @@ window.NEO_CONFIG = {
     "chainId": "0x89" // Polygon
   },
   "infura": {
-    "apiKey": "f2ccdb97a5534c03af03309a48d8a7c2" // API key da imagem
+    "apiKey": "" // Must be configured via server/env dynamically
   }
 };
